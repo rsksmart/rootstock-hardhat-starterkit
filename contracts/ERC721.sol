63c27197e5a6c0cff08ea7a8401c8fe90b1833aa
RookieCol <81944029+RookieCol@users.noreply.github.com>
2025-07-16 04:17:03 +0000

    chore: upgrade to latest supported solidity version

--- contracts/ERC721.sol
+++ contracts/ERC721.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.24;
+pragma solidity 0.8.25;
 
 import { Strings } from "@openzeppelin/contracts/utils/Strings.sol";
 import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
